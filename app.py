a71d40470bf9c868d8f3fdd05212c331de143cf7
kramstyles <mark.eke@decagon.dev>
2022-08-06 23:38:16 +0100

    added flask migrate

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.debug = True
+
+# Code to connect to the database. dialect->username->password->host&port->dbname
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:*@localhost:5432/db_udacity'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
+migrate = Migrate(app,db)
 
 
 class Person(db.Model):
@@ -13,10 +18,17 @@ class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
 
+    def __repr__(self):
+        return self.name
+
+
+db.create_all()
+
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    person = Person.query.first()
+    return f'Hello, {person.name}!'
 
 
 if __name__ == '__main__':
